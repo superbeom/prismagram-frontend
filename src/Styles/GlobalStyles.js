47a96f2b7173826a504ff2c867ccac610216a982
superbeom <it.super.beom@gmail.com>
2019-05-17 17:51:24 +0900

    #4.4 First Hooks Query

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -4,6 +4,14 @@ import reset from "styled-reset";
 export default createGlobalStyle`
     ${reset}
     * {
-        box-sizing: border-box
+        box-sizing: border-box;
+    }
+    body {
+        background-color: ${props => props.theme.bgColor};
+        color: ${props => props.theme.blackColor};
+    }
+    a {
+        color: ${props => props.theme.blueColor};
+        text-decoration: none;
     }
 `;
