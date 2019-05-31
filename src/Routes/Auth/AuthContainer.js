43d11e3e2fdb99fe43bdcd20122915f68c88a448
superbeom <it.super.beom@gmail.com>
2019-05-31 11:01:18 +0900

    #5.5 Toastify and createAccount Mutation

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,22 +2,50 @@ import React, { useState } from "react";
 import useInput from "../../Hooks/useInput";
 import AuthPresenter from "./AuthPresenter";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN } from "./AuthQueries";
+import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import { toast } from "react-toastify";
 
 export default () => {
   const [action, setAction] = useState("logIn");
   const username = useInput("");
   const firstName = useInput("");
   const lastName = useInput("");
-  const email = useInput("");
+  const email = useInput("nouser@daum.net");
+
   const requestSecret = useMutation(LOG_IN, {
+    update: (_, { data }) => {
+      const { requestSecret } = data;
+      if (!requestSecret) {
+        toast.error("You don't have an account yet. Create one!");
+        setTimeout(() => setAction("signUp"), 2000);
+      }
+    },
     variables: { email: email.value }
   });
 
-  const onLogin = e => {
+  const createAccount = useMutation(CREATE_ACCOUNT, {
+    variables: {
+      username: username.value,
+      email: email.value,
+      firstName: firstName.value,
+      lastName: lastName.value
+    }
+  });
+
+  const onSubmit = e => {
     e.preventDefault();
-    if (email !== "") {
-      requestSecret();
+    if (action === "logIn") {
+      if (email.value !== "") {
+        requestSecret();
+      } else {
+        toast.error("Email is required");
+      }
+    } else if (action === "signUp") {
+      if (email.value !== "" && username.value !== "") {
+        createAccount();
+      } else if (email.value !== "" || username.value !== "") {
+        toast.error("Email and Username fields are required");
+      }
     }
   };
 
@@ -29,7 +57,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
-      onLogin={onLogin}
+      onSubmit={onSubmit}
     />
   );
 };
