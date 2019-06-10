0ba5272b277f0c2a7c6c9531c85f4c7d3d23cc4e
superbeom <it.super.beom@gmail.com>
2019-06-10 14:28:20 +0900

    #7.0 Getting the Feed and Loader Component

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,48 @@
-export default () => "Feed";
+import React from "react";
+import { useQuery } from "react-apollo-hooks";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import Loader from "../Components/Loader";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 70vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+
+  return <Wrapper>{loading && <Loader />}</Wrapper>;
+};
