9afe3e2401d7a14de3893699344be880c687b4cd
superbeom <it.super.beom@gmail.com>
2019-06-17 13:56:51 +0900

    #7.3 Post Component part Three

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import useInput from "../../Hooks/useInput";
 import PostPresenter from "./PostPresenter";
@@ -16,7 +16,21 @@ const PostContainer = ({
 }) => {
   const [likeCountS, setLikeCount] = useState(likeCount);
   const [isLikedS, setIsLiked] = useState(isLiked);
+  const [currentItem, setCurrentItem] = useState(0);
   const comment = useInput("");
+
+  const slide = () => {
+    const totalFiles = files.length;
+    if (currentItem === totalFiles - 1) {
+      setTimeout(() => setCurrentItem(0), 2000);
+    } else {
+      setTimeout(() => setCurrentItem(currentItem + 1), 2000);
+    }
+  };
+
+  useEffect(() => {
+    slide();
+  }, [currentItem]);
 
   return (
     <PostPresenter
@@ -31,6 +45,7 @@ const PostContainer = ({
       newComment={comment}
       setLikeCount={setLikeCount}
       setIsLiked={setIsLiked}
+      currentItem={currentItem}
     />
   );
 };
--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
 import { HeartFull, HeartEmpty, Comment } from "../Icons";
@@ -27,11 +28,27 @@ const Location = styled.span`
   font-size: 12px;
 `;
 
-const Files = styled.div``;
+const Files = styled.div`
+  position: relative;
+  padding-bottom: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  flex-shrink: 0;
+`;
 
 const File = styled.img`
   min-width: 100%;
   max-width: 100%;
+  width: 100%;
+  height: 600px;
+  position: absolute;
+  top: 0;
+  background-image: url(${props => props.src}});
+  background-size: cover;
+  background-position: center;
+  opacity: ${props => (props.showing ? 1 : 0)};
+  transition: opacity 0.5s linear;
 `;
 
 const Meta = styled.div`
@@ -62,13 +79,25 @@ const Timestamp = styled.span`
   border-bottom: ${props => props.theme.lightGreyColor} 1px solid;
 `;
 
+const Textarea = styled(TextareaAutosize)`
+  border: none;
+  width: 100%;
+  resize: none;
+  font-size: 14px;
+  &:focus {
+    outline: none;
+  }
+`;
+
 export default ({
   user: { username, avatar },
   location,
   files,
   likeCount,
   isLiked,
-  createdAt
+  createdAt,
+  newComment,
+  currentItem
 }) => {
   const year = createdAt.substring(0, 4);
   const month = createdAt.substring(5, 7);
@@ -86,7 +115,14 @@ export default ({
         </UserColumn>
       </Header>
       <Files>
-        {files && files.map(file => <File key={file.id} src={file.url} />)}
+        {files &&
+          files.map((file, index) => (
+            <File
+              key={file.id}
+              src={file.url}
+              showing={index === currentItem}
+            />
+          ))}
       </Files>
       <Meta>
         <Buttons>
@@ -101,6 +137,7 @@ export default ({
         <Timestamp>
           {year}년 {month}월 {day}일 {hour > 23 ? hour - 24 : hour}:{min}
         </Timestamp>
+        <Textarea placeholder="Add a comment..." {...newComment} />
       </Meta>
     </Post>
   );
